8ab88827afe43b79810a23ea11c04c0d2d84cea2
YiJeongan <wjddks110@kookmin.ac.kr>
2023-11-27 22:01:38 +0900

    add routing, add memberpage

--- project-front/src/App.js
+++ project-front/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import ActivityPage from './pages/ActivityPage'
+import MemberPage from './pages/MemberPage'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      
+          <div className="App">
+            <header>
+              <img 
+                className ='logo'src='img/Logo_mini.png'/> 
+              <nav>
+                <ul>
+                  <li><Link to="/activities" style={{ textDecoration: "none", color: '#747474'}}>Activities</Link></li>
+                  <li><Link to="/members" style={{ textDecoration: "none" , color: '#747474'}}>Member</Link></li>
+                  <li><Link to="/timeline" style={{ textDecoration: "none", color: '#747474' }}>TimeLine</Link></li>
+                  <li><Link to="/contact" style={{ textDecoration: "none", color: '#747474' }}>Contact</Link></li>
+                </ul>
+              </nav>
+            </header>
+
+            <Routes>
+              <Route path="/activities" element={<ActivityPage />} />
+              <Route path="/members" element={<MemberPage />} />
+            </Routes>
+          </div>
+          
+    </Router>
+
   );
 }
 
