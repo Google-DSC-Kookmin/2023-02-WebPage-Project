10998bfbe9333b217e93fd6a896bca9da6a37906
YiJeongan <wjddks110@kookmin.ac.kr>
2024-01-08 20:12:37 +0900

    edit css

--- project-front/src/components/Profile.jsx
+++ project-front/src/components/Profile.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import s from "../components/Style/memberPage.module.css";
 
 const Profile = ({ data }) => {
-    const { Name, Position, Introduction, GitHubLink, Email, Image } = data;
+    const { Name, Position, Introduction, GitHubLink, Email, Image,Responsibility } = data;
 
+
+    const renderPositions = () => {
+        return (
+            <div className={s.dutyContainer}>
+                {Position.map((position, index) => (
+                    <span key={index} className={s.duty}>{position}</span>
+                ))}
+            </div>
+        );
+    };
+    
     return (
         <>
             <div className={s.profileCard}>
@@ -12,14 +23,14 @@ const Profile = ({ data }) => {
                         <img src={Image} className={s.profile_image} alt="Profile" />
                     </main>
 
-                    <span className={s.duty}>{Position}</span>
+                    {renderPositions()}
                     <a href={GitHubLink} target="_blank" rel="noopener noreferrer">
                         <img className={s.github_icon} src='img/githubicon.png' alt="GitHub" />
                     </a>
                 </header>
                 <main>
                     <span className={s.name}>{Name}</span>
-                    <span className={s.position}>{Position}</span>
+                    <span className={s.position}>{Responsibility}</span>
                     <span className={s.ment}>{Introduction}</span>
                 </main>
                 <footer>
--- project-front/src/pages/MemberPage.jsx
+++ project-front/src/pages/MemberPage.jsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import Profile from '../components/Profile';
 import s from '../components/Style/memberPage.module.css';
-import jsonData from '../Data/memberData.json'; // 파일 확장자(.json) 확인
+import jsonData23_24 from '../Data/memberData23_24.json'; 
+import jsonData22_23 from '../Data/memberData22_23.json'; 
 
 const MemberPage = () => {
   const [data, setData] = useState([]);
+  const [data22, setData22] = useState([])
 
   useEffect(() => {
     try {
       // jsonData가 배열이거나 배열로 파싱할 수 있는 경우
-      if (Array.isArray(jsonData)) {
-        setData(jsonData);
+      if (Array.isArray(jsonData23_24)) {
+        setData(jsonData23_24);
+      } else {
+        throw new Error('JSON 데이터가 배열 형태가 아닙니다.');
+      }
+    } catch (error) {
+      console.error('데이터 설정 중 오류 발생:', error.message);
+      // 오류를 적절히 처리하여 데이터의 기본값을 설정하거나 오류 메시지를 표시하는 등의 작업을 할 수 있습니다.
+    }
+    try {
+      // jsonData가 배열이거나 배열로 파싱할 수 있는 경우
+      if (Array.isArray(jsonData22_23)) {
+        setData22(jsonData22_23);
       } else {
         throw new Error('JSON 데이터가 배열 형태가 아닙니다.');
       }
@@ -20,16 +33,30 @@ const MemberPage = () => {
     }
   }, []);
 
-  return (
-    <div>
-      <img className={s.member_main_image} src="img/Group54.png" alt="Main" />
-      <div className={s.profiles}>
-        {data.map((item, index) => (
-          <Profile key={index} data={item} />
+
+  const renderProfiles = (dataArray) => {
+    return dataArray.map((data, index) => (
+      <div key={index} className={s.profiles}>
+        {data.map((item, i) => (
+          <Profile key={i} data={item} />
         ))}
       </div>
-    </div>
-  );
+    ));
+  };
+
+    return (
+      <>
+        <div>
+          <span className={s.main_image}>
+            <img className={s.member_main_image} src="img/Group54.png" alt="Main" />
+          </span>
+          <span className={s.num_member}>23-24</span>
+          {renderProfiles([data])}
+          <span className={s.num_member}>22-23</span>
+          {renderProfiles([data22])}
+        </div>
+      </>
+    );
 };
 
 export default MemberPage;
