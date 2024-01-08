7c7820cd3fee57b727b59246434e2750ad00509d
정일형 <117133787+JohnJung-1017@users.noreply.github.com>
2024-01-08 22:09:12 +0900

    responsive_edit1
    contact,timeline page 반응형으로 수정
    activity page 상단 반응형으로 수정
    seminar, session, expect page만 수정하면 됨.
    css파일 clean coding 진행중

--- project-front/src/pages/ActivityPage.jsx
+++ project-front/src/pages/ActivityPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
 import textStyle from "../components/Style/text.module.css";
-import customStyle1 from "../components/Style/styles.module.css";
+import customStyle1 from "../components/Style/page2.module.css";
 
 import Seminars from "../components/Seminars";
 import Sessions from "../components/Sessions";
@@ -11,14 +11,14 @@ import Expect from "../components/Expect";
 const ActivityPage = () => {
   return (
       <>
-          <div className={customStyle1.greenBox}>
+          <div className={customStyle1.actBackground}>
               <div className={customStyle1.act}>Activities</div>
               <nav className={customStyle1.nav1}>
                   <ul className={customStyle1.nav__link}>
-                      <a className={customStyle1.nav__linkS}>
+                      <a className={customStyle1.nav__linkS1}>
                           Seminars
                       </a>
-                      <a className={customStyle1.nav__linkS}>
+                      <a className={customStyle1.nav__linkS2}>
                           Sessions
                       </a>
                   </ul>
