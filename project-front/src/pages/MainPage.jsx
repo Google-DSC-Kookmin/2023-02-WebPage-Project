e330946e9f497c2428424d376c01edd855de97cf
sehyun.kim <sehyunapple@naver.com>
2024-02-22 20:47:44 +0900

    Fix : SPA 이슈 해결

--- project-front/src/pages/MainPage.jsx
+++ project-front/src/pages/MainPage.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 
 import style from "../components/Style/text.module.css";
+import {useNavigate} from "react-router-dom";
 
 const MainPage = () => {
+    const redirectNavigate = useNavigate();
+
+    useEffect(() => {
+        const search = window.location.search;
+        const params = new URLSearchParams(search);
+
+        params.forEach((value, key) => {
+            console.log(key, value)
+            if(key === "redirect_path") {
+                console.log("Check!")
+                redirectNavigate(value);
+            }
+        });
+    }, []);
+
     return (
     <>
         <Box textAlign={"center"}>
